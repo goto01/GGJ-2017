c38efbcec72d1fae9e3465aa54dc75c89f6d3b5e
goto01 <kirill.tkachev.ktkach@gmail.com>
2017-01-20 21:52:55 +0300

    + Character animator

--- GGJ2017/Assets/Scripts/Components/Movement/MainCharacterMovement/MainCharacterMovementObject.cs
+++ GGJ2017/Assets/Scripts/Components/Movement/MainCharacterMovement/MainCharacterMovementObject.cs
@@ -7,6 +7,9 @@ namespace Assets.Scripts.Components.Movement.MainCharacterMovement
 {
     class MainCharacterMovementObject : BaseMovementObject
     {
+        private readonly int _horizontalAxis = Animator.StringToHash("Horizontal axis");
+        private readonly int _verticalAxis = Animator.StringToHash("Vertical axis");
+
         [SerializeField] private List<DirectionDescription> _descriptions;
         
         protected override Vector3 Offset
@@ -21,15 +24,27 @@ namespace Assets.Scripts.Components.Movement.MainCharacterMovement
         {
             get
             {
-                var direction = default(Vector2);
-                foreach (var directionDescription in _descriptions)
-                {
-                    if (InputController.Instance.CheckInput(directionDescription.InputName))
-                        direction += directionDescription.Direction;
-                }
-                Debug.Log(direction);
+                var direction = GetInputDirection();
+                UpdateAnimator(direction);
                 return Vector2Helper.ClampVector(direction, -1, 1);
             }
+        }
+
+        private Vector2 GetInputDirection()
+        {
+            var direction = default(Vector2);
+            foreach (var directionDescription in _descriptions)
+            {
+                if (InputController.Instance.CheckInput(directionDescription.InputName))
+                    direction += directionDescription.Direction;
+            }
+            return direction;
+        }
+
+        private void UpdateAnimator(Vector2 direction)
+        {
+            _animator.SetFloat(_horizontalAxis, direction.x);
+            _animator.SetFloat(_verticalAxis, direction.y);
         }
     }
 }
