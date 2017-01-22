aa03ee1f443bbf812e6430b757f6863d8bee0da8
goto01 <kirill.tkachev.ktkach@gmail.com>
2017-01-22 04:01:33 +0300

    + Input / Output system

---
+++ GGJ2017/Assets/Scripts/Components/InpputOutputSoundSystem/InputSound.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts.Components.StageComponents.Waves;
+using Assets.Scripts.Components.StageComponents.Waves.WaveItems;
+using Assets.Scripts.Staff.Core;
+using UnityEngine;
+
+namespace Assets.Scripts.Components.InpputOutputSoundSystem
+{
+    class InputSound : BindingMonoBehaviour
+    {
+        private const string Tag = "Sound input";
+
+        [SerializeField] private List<OutputSound> _outputs;
+
+        protected virtual void OnTriggerEnter2D(Collider2D coll)
+        {
+            if (coll.tag.Equals(BaseWave.Tag)) HandleSound(coll.GetComponent<WaveItem>());
+        }
+
+        private void HandleSound(WaveItem item)
+        {
+            Debug.Log(item.name);
+            Debug.Log(item is StreightWaveItem);
+            if (item is StreightWaveItem) HandleStreightWave();
+            else HandleSimpleWave();
+        }
+
+        private void HandleSimpleWave()
+        {
+            foreach (var output in _outputs) output.GenerateSimpleWave();
+        }
+
+        private void HandleStreightWave()
+        {
+            foreach (var output in _outputs) output.GenerateStreightWave();
+        }
+    }
+}
---
+++ GGJ2017/Assets/Scripts/Components/InpputOutputSoundSystem/OutputSound.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Components.StageComponents.Waves.WaveItems;
+using Assets.Scripts.Staff.Pool;
+using UnityEngine;
+
+namespace Assets.Scripts.Components.InpputOutputSoundSystem
+{
+    class OutputSound : MonoBehaviour
+    {
+        [SerializeField] private PortablePool _simpleWaves;
+        [SerializeField] private PortablePool _streightWaves;
+        [SerializeField] private Vector2 _streightWaveDirection;
+
+        public void GenerateSimpleWave()
+        {
+            var position = transform.position;
+            var o = _simpleWaves.PopObject();
+            position.z = o.transform.position.z;
+            o.transform.position = position;
+        }
+
+        public void GenerateStreightWave()
+        {
+            var position = transform.position;
+            var o = _streightWaves.PopObject();
+            position.z = o.transform.position.z;
+            o.transform.position = position;
+            o.GetComponent<StreightWaveItem>().Direction = _streightWaveDirection;
+        }
+    }
+}
